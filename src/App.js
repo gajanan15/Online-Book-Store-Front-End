4c134ef3e2c8e36bdca8e66cdf38490d98a6b171
gajanan15 <gajanandesai97@gmail.com>
2020-05-01 13:46:38 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Welcome To CB's Book Store  
+    </div>
+  );
+}
+
+export default App;
