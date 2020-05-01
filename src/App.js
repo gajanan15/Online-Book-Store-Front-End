1cfbc9595f52d785fe424f295037c635560182e1
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-05-01 19:27:15 +0530

    ADDED: Footer's section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import CbHeader from './Component/CbHeader'
+import CbFooter from './Component/CbFooter'
+
 
 function App() {
   return (
     <div className="App">
         <CbHeader />
+        <CbFooter />
     </div>
   );
 }
---
+++ src/Component/CbFooter.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import '../Css/CbHeader.css';
+
+export class CbFooter extends Component {
+    render() {
+        return (
+            <div>
+                <div className="footer">
+                    <p> &copy; Copyright 2020, The CB's BookStore Pvt Ltd. <br />
+                        All Rights Reserved</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CbFooter
