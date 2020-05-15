46b24374e9c580a967d87813d861e2867fec96c4
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-05-15 21:31:28 +0530

    ADDED : Search bar in homepage

--- src/component/utils/CbHeader.js
+++ src/component/utils/CbHeader.js
@@ -4,6 +4,9 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
 import '../../css/CbHeader.css';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from "@material-ui/core/InputBase";
+
 
 
 export class CbHeader extends Component {
@@ -21,6 +24,19 @@ export class CbHeader extends Component {
                         <Typography id="title" variant="h6" noWrap>
                             CB's Book Store
                         </Typography>
+
+                        <div className="search">
+                            <div className="searchIcon">
+                                <SearchIcon/>
+                            </div>
+                            <InputBase
+                                placeholder="Search..."
+                                className="inputRoot inputInput"
+                                inputProps={{'aria-label': 'search'}}
+                                onChange={(event) => this.getText(event)}
+                            />
+                        </div>
+
                     </Toolbar>
                 </AppBar>
             </div>
