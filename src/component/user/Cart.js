5785c4f3483aac56467eacde5b507b8e995aa140
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-05-17 23:09:12 +0530

    ADDED: Buttons to update book's quantity

--- src/component/user/Cart.js
+++ src/component/user/Cart.js
@@ -5,6 +5,9 @@ import CbHeader from "../utils/CbHeader";
 import Container from "@material-ui/core/Container";
 import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
 
 class Cart extends Component {
     render() {
@@ -15,16 +18,26 @@ class Cart extends Component {
                     <Card className={"bookdiv"} variant="outlined">
                         <h4>My Cart (0)</h4>
                         <div>
-                            <img src={require("../../asset/Bleeds.jpg")} style={{height:"16vh"}}/>
+                            <img src={require("../../asset/Bleeds.jpg")} style={{height: "16vh"}}/>
                         </div>
-                        <div className="booksContainer" style={{marginLeft: "-1%",marginBottom:"-1.5%"}}>
+                        <div className="booksContainer" style={{marginLeft: "-1%", marginBottom: "-1.5%"}}>
                             <Typography component="h2" id="bookname1">Bleeds</Typography>
                             <Typography variant="body2" color="textSecondary" id="authorName">CJ Miller</Typography>
                             <Typography component="h2" id="cost">Rs.50</Typography>
                         </div>
+                        <div className="plusminusdiv">
+                            <IconButton id="minus">
+                                <RemoveCircleOutlineIcon style={{fontSize: "90%", color: "rgb(165,42,42)"}}/>
+                            </IconButton>
+                            <input id="text" value="1"></input>
+                            <IconButton className="plus">
+                                <AddCircleOutlineIcon style={{fontSize: "90%", color: "rgb(165,42,42)"}}/>
+                            </IconButton>
+                        </div>
                         <Button id="orderBtn">
                             Continue
                         </Button>
+
                     </Card>
                 </Container>
                 <CbFooter/>
