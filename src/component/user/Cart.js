6218d8020a83e1169127ddf282ce5e2f32587ef5
junaidansari6311 <junaidansari6311@gmail.com>
2020-05-20 15:00:28 +0530

    ADDED: Css for order summary details

--- src/component/user/Cart.js
+++ src/component/user/Cart.js
@@ -235,7 +235,7 @@ class Cart extends Component {
                             }
                         </div>
                         <Button onClick={this.handleCustomer}
-                                style={cartData.length < 0 ? {visibility: "hidden"} : {visibility: this.state.btn1}}
+                                style={cartData.length === 0 ? {visibility: "hidden"} : {visibility: this.state.btn1}}
                                 id="orderBtn">
                             Continue
                         </Button>
