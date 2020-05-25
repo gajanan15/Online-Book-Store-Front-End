f818defd11e29e01d8d972b81841e4dea6da0d3c
kalyaniRane <kalyanirane19@gmail.com>
2020-05-25 23:57:54 +0530

    ADDED : Card to display login and signup option

--- src/component/user/SignUp.js
+++ src/component/user/SignUp.js
@@ -7,6 +7,19 @@ class SignUp extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            loginChecked: true,
+            signupChecked: false
+        }
+    }
+
+    handleTabSelection = ({target}) => {
+        if ([target.name].includes("login")) {
+            this.setState({loginChecked: true, signupChecked: false})
+        }
+        if ([target.name].includes("signup")) {
+            this.setState({loginChecked: false, signupChecked: true})
+        }
     }
 
     render() {
@@ -19,6 +32,19 @@ class SignUp extends Component {
                             <h3 style={{fontWeight: "bold"}}>
                                 THE CB'S BOOK STORE
                             </h3>
+                        </div>
+                    </Card>
+
+                    <Card className="login-box" style={{borderRadius: "2%", boxShadow: "0 8px 16px 0 rgba(0,0,0,0.5)"}}>
+                        <div className="login-wrap">
+                            <div className="login-html">
+                                <input id="tab-1" type="radio" name="login" className="sign-in"
+                                       checked={this.state.loginChecked} onClick={this.handleTabSelection}/><label
+                                htmlFor="tab-1" className="tab1">Login</label>
+                                <input id="tab-2" type="radio" name="signup" className="sign-up"
+                                       checked={this.state.signupChecked} onClick={this.handleTabSelection}/>
+                                <label htmlFor="tab-2" className="tab2">SignUp</label>
+                            </div>
                         </div>
                     </Card>
                 </div>
