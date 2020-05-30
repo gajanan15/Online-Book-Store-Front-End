caf764321dca66e6882733bfc6e077d0dfad25fb
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-05-30 17:48:27 +0530

    ADDED : Cart to display login and signup option for user

--- src/component/utils/CbHeader.js
+++ src/component/utils/CbHeader.js
@@ -11,6 +11,10 @@ import Badge from "@material-ui/core/Badge";
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import PersonOutlineSharpIcon from '@material-ui/icons/PersonOutlineSharp';
 import {Link} from 'react-router-dom'
+import CardContent from "@material-ui/core/CardContent";
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
 
 export class CbHeader extends Component {
     constructor(props) {
@@ -19,7 +23,13 @@ export class CbHeader extends Component {
             searchField: [],
             tempData: [],
             searchVisibility: false,
-            counter: 0
+            counter: 0,
+            visibilityValueOfLogin: 'hidden',
+            visibilityOfDialogBox: false,
+            visibilityOfCloseIcon: 'hidden',
+            userLoggedIn: false,
+            logorsign:"LOGIN/SIGNUP",
+            redirect:"user/login"
         }
     }
 
@@ -31,6 +41,32 @@ export class CbHeader extends Component {
 
     getText = (event) => {
         this.props.test(event.target.value)
+    }
+
+    handleDialogueBoxVisibility = (user) => {
+        if (user === true) {
+            window.location.reload(true)
+            localStorage.removeItem('Authorization')
+        }
+        if (user === false) {
+            this.setState({
+                visibilityOfDialogBox: true,
+                visibilityOfCloseIcon: "visible",
+                visibilityValueOfLogin: "hidden"
+            })
+        }
+    }
+
+    handleLoginBoxVisibility = (event) => {
+
+        if (`${this.state.visibilityValueOfLogin}` === "hidden") {
+            this.setState({visibilityValueOfLogin: "visible"})
+            return;
+        }
+        if (`${this.state.visibilityValueOfLogin}` === "visible") {
+            this.setState({visibilityValueOfLogin: "hidden"})
+            return;
+        }
     }
 
     handleBadgeCount(value, updateFactor) {
@@ -84,6 +120,28 @@ export class CbHeader extends Component {
                         <PersonOutlineSharpIcon className="userIcon"/>
                     </Toolbar>
                 </AppBar>
+
+                {!this.state.userLoggedIn ?
+                    <Card className="loginsignupcard" style={{visibility: this.state.visibilityValueOfLogin}} variant="outlined">
+                        <CardContent><Typography id="mainName" style={{fontWeight: "bold"}}>Welcome</Typography>
+                            <Typography id="subName" color="textSecondary" gutterBottom>To
+                                access the account and manage orders</Typography>
+                            <Button className="loginorsignupbutton"
+                                    onClick={() => this.handleDialogueBoxVisibility(this.state.userLoggedIn)}>{this.state.logorsign}</Button>
+                        </CardContent>
+                    </Card>
+                    :
+                    <Card  className="loginsignupcard1" style={{visibility: this.state.visibilityValueOfLogin}} variant="outlined">
+                        <CardContent><Typography id="userName-logincart">Hello, {this.props.name}</Typography>
+                            <Typography id="order-title" color="textSecondary" ><CardGiftcardIcon id="order-icon"/>
+                                <Link to={"/orders"}>My Orders</Link>
+                            </Typography>
+                            <Button className="loginorsignupbutton"
+                                    onClick={() => this.handleDialogueBoxVisibility(this.state.userLoggedIn)}>{this.state.logorsign}</Button>
+                        </CardContent>
+                    </Card>
+                }
+
             </div>
         )
     }
