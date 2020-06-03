b675101d9f5ea1e2a17c3a24c1b9e787a7496746
gajanan15 <gajanandesai97@gmail.com>
2020-06-04 00:55:32 +0530

    ADDED : Validation for resend email page

--- src/component/user/ResendEmail.js
+++ src/component/user/ResendEmail.js
@@ -3,18 +3,45 @@ import CbHeader from "../utils/CbHeader";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import {ThemeProvider} from "@material-ui/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import {createMuiTheme} from "@material-ui/core/styles";
 import '../../css/ForgotPassword.css'
 
 class ResendEmail extends Component {
     constructor(props){
         super(props);
         this.state={
-            emailID:""
+            resendEmailId:' ',
+            resendEmailError:'',
+            resendEmailID:"",
+            error:'',
+            errorStatus:false,
+            emailID:"",
+            snackFlag: false,
+            severity: "error",
+            snackMessage: ""
         }
     }
 
+    emailValidation=(event,error)=>{
+        let emailPattern="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
+        if(!event.target.value.match(emailPattern)){
+            this.setState({
+                [event.target.id]: "Enter valid email id",
+                [error]: `Invalid ${event.target.name}`,
+                errorStatus: true,
+            })
+        }
+        else {
+            this.setState({
+                [event.target.id]: " ",
+                [error]:"",
+                errorStatus: false,
+            })
+        }
+    }
 
     changeState = (event) => {
         this.setState({
@@ -23,6 +50,14 @@ class ResendEmail extends Component {
     }
 
     render() {
+
+        const theme = createMuiTheme({
+            palette: {
+                primary: {
+                    main: '#a52a2a',
+                },
+            },
+        });
 
         return (
             <div>
@@ -37,6 +72,7 @@ class ResendEmail extends Component {
                                         Enter your email address and we'll send you a email verification link.
                                     </Typography>
                                     <div id="forgotEmailText">
+                                        <ThemeProvider theme={theme}>
                                             <TextField id="resendEmailId"
                                                        name="emailID"
                                                        label="Email Id"
@@ -44,12 +80,16 @@ class ResendEmail extends Component {
                                                        value={this.state.emailID}
                                                        fullWidth required autoComplete="off"
                                                        onChange={this.changeState}
+                                                       error={this.state.resendEmailError}
+                                                       onBlur={(e)=>this.emailValidation(e,"resendEmailError")}
+                                                       helperText={this.state.resendEmailId}
                                             />
+                                        </ThemeProvider>
                                     </div>
                                     <div>
                                         <Button id="resetbtn" style={this.state.emailID === "" ?
                                             {backgroundColor: "#d3d3d3", pointerEvents: "none",color:"black"} :
-                                            {backgroundColor: "#a03037"}}>Resend</Button>
+                                            {backgroundColor: "#a03037"}} onClick={this.handleButton}>Resend</Button>
                                     </div>
                                 </div>
                             </CardContent>
