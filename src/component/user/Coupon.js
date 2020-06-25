ce991601cc2ae88d9ce1871391ecb30c98fd7d02
gajanan15 <gajanandesai97@gmail.com>
2020-06-25 20:50:00 +0530

    ADDED : Component for coupon feature

---
+++ src/component/user/Coupon.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Coupon extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div className="coupon-hero">
+                <div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Coupon;
