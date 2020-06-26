7aa95e9f107ba143fc9b874bbd135d73c8948953
gajanan15 <gajanandesai97@gmail.com>
2020-06-26 15:07:22 +0530

    ADDED : Card to display the coupons

--- src/component/user/Coupon.js
+++ src/component/user/Coupon.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
 
 class Coupon extends Component {
 
@@ -11,6 +13,13 @@ class Coupon extends Component {
         return (
             <div className="coupon-hero">
                 <div>
+                    <Card className="coupon-box">
+                        <div style={{height:"50%",backgroundColor:"brown"}}>
+                            <Typography style={{color:"#fff",fontSize:"20px",marginLeft:"5%",marginTop:"1%"}}>
+                                Coupons
+                            </Typography>
+                        </div>
+                    </Card>
 
                 </div>
             </div>
